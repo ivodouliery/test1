434acd7db55fce1556f1080ce280961e39f62b34
Ivo Douliery <ivodoul@gmail.com>
2020-02-15 23:34:54 +0100

    2ème save

--- test1.c
+++ test1.c
@@ -2,14 +2,23 @@
 
 
 int Somme(int n) {
-    int i=0,total=0; for(i=1;i<=n;i++)
-    total = total + i; return (total);
+    int i=0,total=0; 
+    for(i=1;i<=n;i++){
+    total = total + i; 
+    }
+    return (total);
 }
 
+int carre(int a){
+    return a*a;
+}
 
 int main() {
     int x=0;
     printf("Donnez un entier positif: ");
     scanf("%d",&x);
-    printf("La somme des entiers de 1 a %d est %d\n",x,Somme(x)); return 0;
+    printf("La somme des entiers de 1 a %d est %d\n",x,Somme(x)); 
+    printf("le carre de %d est %d\n",x,carre(x));
+    return 0;
 }
+
